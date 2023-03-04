00e32cf72dfae87e7606c89d769542020fd46bf9
Vòng Chí Văn <48863061+ChiVan222@users.noreply.github.com>
2023-03-05 00:15:23 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.balloons()
+st.snow()
+st.title('Giải phương trình bậc nhất')
+a = st.number_input('Tham số a' )
+b = st.number_input('Tham số b')
+if st.button('Giải'):
+  if(a==0 and b != 0) : 
+    st.success("Không có x nào là nghiệm")
+  else : 
+    if(a==0 and b==0) :
+     st.success("Có vô số x thỏa là nghiệm")
+    else : 
+      st.success("Phương trình có nghiệm là",-b/a)
